22523ad51f85622d702f8c3d28330366f5d19c20
Najwanugroho <146321679+Najwanugroho@users.noreply.github.com>
2023-09-28 17:29:05 +0700

    Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("NAMA: Najwa Shaqila Isna Nugroho");
+        System.out.println("NIM : 607062300125");
+        System.out.println("Kelas: D3IF-47-01");
+    }
+}
